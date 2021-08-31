c337a6b4f66a90a70a972bf758bd0d729a67cf23
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-08-31 17:49:35 +0100

    Completed routes and basic pages

---
+++ front-end/src/pages/Appointments.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Appointments() {
+    return (
+        <h2>Appointments</h2>
+    )
+}
+
+export default Appointments
---
+++ front-end/src/pages/Counsellor.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Counsellor() {
+    return (
+        <h2>Counsellor</h2>
+    )
+}
+
+export default Counsellor
