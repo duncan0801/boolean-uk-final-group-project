2f018a3ee4ae86fcde37d187a91332505789980e
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-09-02 09:59:25 +0100

    working on counsellor page

--- front-end/src/components/CounsellorCard.tsx
+++ front-end/src/components/CounsellorCard.tsx
@@ -1,10 +1,15 @@
 import react from "react"
+import "../styles/counsellor-card.css"
 
 function CounsellorCard() {
     return (
-        <div>
-            <h3>Jane Doe</h3>
-        </div>
+        <article className="counsellor-card">
+            <img className="counsellor-image" src="https://avatars.githubusercontent.com/u/1071625?v=4"
+            alt="John Doe"/>
+            <h3 className="counsellor-name">John Doe</h3>
+            <h4>Hypnotherapy, Anxiety, Depression</h4>
+            <p>rating</p>
+        </article>
     )
 }
 
