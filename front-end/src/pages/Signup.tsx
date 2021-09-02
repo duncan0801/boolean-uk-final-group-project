ca408d360b3d5f8c7352987d7f11136f2d0aaf54
tati89 <malena.one@gmail.com>
2021-09-02 10:02:35 +0100

    header, login,create page layout done

--- front-end/src/pages/Signup.tsx
+++ front-end/src/pages/Signup.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import "../styles/signup.css";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+        width: "25ch",
+      },
+    },
+  })
+);
 
 function Signup() {
-    return (
-        <h2>Signup</h2>
-    )
+  const classes = useStyles();
+  return (
+    <div className="sighn-up-container">
+      <h1 className="sign-up-page-header">Create your account</h1>
+
+      <div className="sign-up-form-wrapper">
+        <form className="sign-up-form" noValidate autoComplete="off">
+          <div className="first-last-names-form ">
+            <TextField id="firstName" label="Name" variant="outlined" />
+            <TextField id="lastName" label="Surname" variant="outlined" />
+          </div>
+          <TextField
+            id="userName"
+            label="username"
+            variant="outlined"
+            placeholder="Username"
+          />
+          <TextField id="password" label="password" variant="outlined" />
+          <TextField id="avatar" label="avatar" variant="outlined" />
+          <div className="sign-up-button-wrapper">
+            <button className="sign-up-button">
+              <Link to="/services" className="get-started-link">
+                Get started
+              </Link>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
