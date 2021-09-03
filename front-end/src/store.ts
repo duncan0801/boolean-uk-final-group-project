3d0eda836f4b7b0914f22ad0b8b5d10a6c2a313b
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-09-03 11:48:10 +0100

    deleted type

--- front-end/src/store.ts
+++ front-end/src/store.ts
@@ -1,33 +1,26 @@
 import create from "zustand";
 import { devtools } from "zustand/middleware";
 
-type Counsellor = {
-    firstName:    String
-    lastName:     String
-	avatar: String
-    specialties:  String[]
-}
-
 type Faq = {
-	id: number;
-	question: string;
-	answer: string;
+  id: number;
+  question: string;
+  answer: string;
 };
 
 type Store = {
-	faqs: Faq[] | null;
-	setFaqs: (faqs: Faq[]) => void;
-	counsellors: Counsellor[] | null;
-	setCounsellors: (counsellors: Counsellor[]) => void;
+  faqs: Faq[] | null;
+  setFaqs: (faqs: Faq[]) => void;
+  counsellors: Counsellor[] | null;
+  setCounsellors: (counsellors: Counsellor[]) => void;
 };
 
 const useStore = create<Store>(
-	devtools((set) => ({
-		faqs: null,
-		setFaqs: (faqs) => set({ faqs: faqs }),
-		counsellors: null,
-		setCounsellors: (counsellors) => set({ counsellors: counsellors }),
-	})),
+  devtools((set) => ({
+    faqs: null,
+    setFaqs: (faqs) => set({ faqs: faqs }),
+    counsellors: null,
+    setCounsellors: (counsellors) => set({ counsellors: counsellors }),
+  }))
 );
 
 export default useStore;
