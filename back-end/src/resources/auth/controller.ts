c6cc7916b4e4e7d6ce20162aa3922f12151f4a6c
tati89 <malena.one@gmail.com>
2021-09-07 17:27:22 +0100

    login is working, add review is working

--- back-end/src/resources/auth/controller.ts
+++ back-end/src/resources/auth/controller.ts
@@ -4,49 +4,49 @@ import { findUserWithValidation, findUnique } from "./service";
 import { createToken, validateToken } from "../../utils/authGenerator";
 
 export const loginUser = async (req: Request, res: Response) => {
-	//  Get user credentials
-	const userCreds: User = req.body;
+  //  Get user credentials
+  const userCreds: User = req.body;
 
-	try {
-		// Check if credentials are valid
-		const loggedUser = await findUserWithValidation(userCreds);
+  try {
+    // Check if credentials are valid
+    const loggedUser = await findUserWithValidation(userCreds);
 
-		// Create token, this will be the user Passport
-		const token = createToken({
-			id: loggedUser.id,
-		});
+    // Create token, this will be the user Passport
+    const token = createToken({
+      id: loggedUser.id,
+    });
 
-		res.cookie("token", token, { httpOnly: true });
+    res.cookie("token", token, { httpOnly: true });
 
-		res.json({ data: { username: loggedUser.username } });
-	} catch (error) {
-		res.status(401).json({ error });
-	}
+    res.json({ data: { username: loggedUser.username } });
+  } catch (error) {
+    res.status(401).json({ error });
+  }
 };
 
 export async function logoutUser(req: Request, res: Response) {
-	res.clearCookie("token");
-	res.json({ msg: "You've been succesfully logged out", data: null });
+  res.clearCookie("token");
+  res.json({ msg: "You've been succesfully logged out", data: null });
 }
 
 export async function validateLoggedInToken(req: Request, res: Response) {
-	const token = req.cookies.token;
+  const token = req.cookies.token;
 
-	const tokenPayload =
-		token && (validateToken(token) as { id: string; role: string });
+  const tokenPayload =
+    token && (validateToken(token) as { id: string; role: string });
 
-	if (tokenPayload) {
-		const userData = await findUnique({
-			where: {
-				id: parseInt(tokenPayload.id),
-			},
-			select: {
-				username: true,
-			},
-		});
+  if (tokenPayload) {
+    const userData = await findUnique({
+      where: {
+        id: parseInt(tokenPayload.id),
+      },
+      select: {
+        username: true,
+      },
+    });
 
-		res.json({ data: userData });
-	} else {
-		res.status(401).json({ err: "No valid token was found" });
-	}
+    res.json({ data: userData });
+  } else {
+    res.status(401).json({ err: "No valid token was found" });
+  }
 }
